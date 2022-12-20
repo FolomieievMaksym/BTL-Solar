0190fb941c4febf1f99f22482c5e990284106088
Folomieiev Maksym <folomieiev.m@gmail.com>
2022-12-20 10:35:59 +0200

    add title

--- docs/mail.php
+++ docs/mail.php
@@ -1,8 +1,37 @@
 <?php
+define('TG_TOKEN', '5766091008:AAEA9Uq5RRpKyk2584Dxhy2b57fjZtKqLPo');
+define('TG_CHAT_ID', '1111958389');
+
+function sendTelegram($msg){
+    $chat_id = TG_CHAT_ID;
+    $disable_web_page_preview = null;
+    $reply_to_message_id = null;
+    $reply_markup = null;
+
+    $data = array(
+        'chat_id' => urlencode($chat_id),
+        'text' => urlencode($msg),
+        'disable_web_page_preview' => urlencode($disable_web_page_preview),
+        'reply_to_message_id' => urlencode($reply_to_message_id),
+        'reply_markup' => urlencode($reply_markup)
+    );
+
+    $url = 'https://api.telegram.org/bot'.TG_TOKEN.'/sendMessage';
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_POST, count($data));
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_exec($ch);
+    curl_close($ch);
+}
+
 $link = "success.php";
 $method = $_SERVER['REQUEST_METHOD'];
 //Script Foreach
 $c = true;
+$message = "";
 if ( $method === 'POST' ) {
 
 	$project_name = "Argus";
@@ -38,8 +67,6 @@ if ( $method === 'POST' ) {
 	}
 }
 
-$message = "$message";
-
 function adopt($text) {
 	return '=?UTF-8?B?'.Base64_encode($text).'?=';
 }
@@ -49,5 +76,13 @@ $headers = "MIME-Version: 1.0" . PHP_EOL .
 'From: '.adopt($project_name).' <'.$admin_email.'>' . PHP_EOL .
 'Reply-To: '.$admin_email.'' . PHP_EOL;
 
+$name = isset($_POST['name']) ? $_POST['name'] : '';
+$phone = isset($_POST['phone']) ? $_POST['phone'] : '';
+$email = isset($_POST['email']) ? $_POST['email'] : '';
+$tg_text = "Имя: $name\nТелефон: $phone\nE-mail: $email";
+sendTelegram($tg_text);
 mail($admin_email, adopt($form_subject), $message, $headers );
+
+
+
 header('Location: '.$link);
