4ccc187a75b3a1b017ecd586c0cf0a0cc4993645
Folomieiev Maksym <folomieiev.m@gmail.com>
2022-12-28 13:00:54 +0200

    up

--- docs/js/spoiler.js
+++ docs/js/spoiler.js
@@ -1,4 +1,20 @@
 // Базовые стили для спойлера
+document.body.addEventListener("click", closeSpoiler);
+
+function closeSpoiler(e) {
+   if (!e.target.closest("[data-spoiler]")) {
+      closeSpoilerFunc();
+   }
+}
+
+function closeSpoilerFunc() {
+   document.querySelectorAll("[data-spoiler]").forEach((el) => {
+      el.children[1].style.height = "0px";
+      el.classList.remove("opened");
+   });
+   window.removeEventListener("scroll", closeSpoilerFunc);
+}
+
 document.querySelectorAll("[data-spoiler]").forEach((el) => {
    el.parentElement.addEventListener("click", toggleSpoiler);
    // el.children[0].style.cursor = "pointer";
@@ -20,9 +36,13 @@ function toggleSpoiler(e) {
          e.target.closest("[data-spoiler]").children[1].style.height = "0px";
          e.target.closest("[data-spoiler]").classList.remove("opened");
       } else {
+         closeSpoilerFunc();
          e.target.closest("[data-spoiler]").children[1].style.height =
             e.target.closest("[data-spoiler]").children[1].scrollHeight + "px";
          e.target.closest("[data-spoiler]").classList.add("opened");
+         window.addEventListener("scroll", closeSpoilerFunc);
       }
+   } else if (e.target.closest("[data-spoiler] a")) {
+      closeSpoilerFunc();
    }
 }
