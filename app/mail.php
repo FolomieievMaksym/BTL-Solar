ef41585b34b02733a2784c9f47e2b66ecae3779d
Folomieiev Maksym <folomieiev.m@gmail.com>
2023-01-11 12:34:09 +0200

    up

--- app/mail.php
+++ app/mail.php
@@ -43,9 +43,9 @@ if ( $method === 'POST' ) {
 	$_POST['form_subject'] = $form_subject;
 	foreach ( $_POST as $key => $value ) {
 		if ( $value != "" && $key != "project_name" && $key != "admin_email" && $key != "form_subject" && $key != "utm1" && $key != "utm2" ) {
-			$message .= "
-			$key: $value;
-			";
+$message .= "
+$key: $value;
+";
 		}
 	}
 } else if ( $method === 'GET' ) {
@@ -60,9 +60,9 @@ if ( $method === 'POST' ) {
 
 	foreach ( $_GET as $key => $value ) {
 		if ( $value != "" && $key != "project_name" && $key != "admin_email" && $key != "form_subject" && $key != "utm1" && $key != "utm2" ) {
-			$message .= "
-			$key: $value;
-			";
+$message .= "
+$key: $value;
+";
 		}
 	}
 }
