f636cc4c6a26bffd968172796fc33cdfe16842b6
Folomieiev Maksym <folomieiev.m@gmail.com>
2023-01-16 17:43:18 +0200

    load 500W img + pdf

---
+++ argus.in.ua/success.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta charset="UTF-8" />
+    <title>УСПЕШНЫЙ ЗАКАЗ!</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="default/css/style.css"/>
+    <link rel="stylesheet" href="default/css/media.css"/>
+	
+<link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,700&amp;subset=cyrillic" rel="stylesheet">
+<!-- Event snippet for Покупка conversion page -->
+<script>
+  gtag('event', 'conversion', {
+      'send_to': 'AW-11029699832/EEHoCJGl2YUYEPi5r4sp',
+      'transaction_id': ''
+  });
+</script>
+<!-- Facebook Pixel Code -->
+    <script>
+          !function (f, b, e, v, n, t, s) {
+              if (f.fbq) return;
+              n = f.fbq = function () {
+                  n.callMethod ?
+                      n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+              };
+              if (!f._fbq) f._fbq = n;
+              n.push = n;
+              n.loaded = !0;
+              n.version = '2.0';
+              n.queue = [];
+              t = b.createElement(e);
+              t.async = !0;
+              t.src = v;
+              s = b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t, s)
+          }(window, document, 'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '692298659166040');
+          fbq('track', 'PageView');
+          fbq('track', 'Lead');
+          fbq('track', 'CompleteRegistration');
+
+      </script>
+      <noscript>
+          <img height="1" width="1" style="display:none"
+               src="https://www.facebook.com/tr?id=692298659166040&ev=PageView&noscript=1"/>
+      </noscript>
+      <!-- End Facebook Pixel Code -->
+</head>
+
+<body>
+
+    <div class="block1">
+        <div class="block_main">
+            <h1 class="text1"><span>ВАША</span> ЗАЯВКА ОФОРМЛЕНА!</h1><br>
+            <p class="text3">Консультант свяжется с Вами в ближайшее время</p>
+            <img class="operator" src="default/images/operator.png" alt="" />
+        </div>
+    </div>
+</body>
+</html>
