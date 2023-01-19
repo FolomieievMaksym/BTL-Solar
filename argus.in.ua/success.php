dfe15e6731a005420e89327b01e2f60365255914
Folomieiev Maksym <folomieiev.m@gmail.com>
2023-01-19 11:33:22 +0200

    fix text

--- argus.in.ua/success.php
+++ argus.in.ua/success.php
@@ -48,6 +48,161 @@
                src="https://www.facebook.com/tr?id=692298659166040&ev=PageView&noscript=1"/>
       </noscript>
       <!-- End Facebook Pixel Code -->
+      <!-- Facebook Pixel Code -->
+    <script>
+          !function (f, b, e, v, n, t, s) {
+              if (f.fbq) return;
+              n = f.fbq = function () {
+                  n.callMethod ?
+                      n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+              };
+              if (!f._fbq) f._fbq = n;
+              n.push = n;
+              n.loaded = !0;
+              n.version = '2.0';
+              n.queue = [];
+              t = b.createElement(e);
+              t.async = !0;
+              t.src = v;
+              s = b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t, s)
+          }(window, document, 'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '906158467123438');
+          fbq('track', 'PageView');
+          fbq('track', 'Lead');
+          fbq('track', 'CompleteRegistration');
+
+      </script>
+      <noscript>
+          <img height="1" width="1" style="display:none"
+               src="https://www.facebook.com/tr?id=906158467123438&ev=PageView&noscript=1"/>
+      </noscript>
+      <!-- End Facebook Pixel Code -->
+      <!-- Facebook Pixel Code -->
+    <script>
+          !function (f, b, e, v, n, t, s) {
+              if (f.fbq) return;
+              n = f.fbq = function () {
+                  n.callMethod ?
+                      n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+              };
+              if (!f._fbq) f._fbq = n;
+              n.push = n;
+              n.loaded = !0;
+              n.version = '2.0';
+              n.queue = [];
+              t = b.createElement(e);
+              t.async = !0;
+              t.src = v;
+              s = b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t, s)
+          }(window, document, 'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '1179969275960877');
+          fbq('track', 'PageView');
+          fbq('track', 'Lead');
+          fbq('track', 'CompleteRegistration');
+
+      </script>
+      <noscript>
+          <img height="1" width="1" style="display:none"
+               src="https://www.facebook.com/tr?id=1179969275960877&ev=PageView&noscript=1"/>
+      </noscript>
+      <!-- End Facebook Pixel Code -->
+      <!-- Facebook Pixel Code -->
+    <script>
+          !function (f, b, e, v, n, t, s) {
+              if (f.fbq) return;
+              n = f.fbq = function () {
+                  n.callMethod ?
+                      n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+              };
+              if (!f._fbq) f._fbq = n;
+              n.push = n;
+              n.loaded = !0;
+              n.version = '2.0';
+              n.queue = [];
+              t = b.createElement(e);
+              t.async = !0;
+              t.src = v;
+              s = b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t, s)
+          }(window, document, 'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '707913527708593');
+          fbq('track', 'PageView');
+          fbq('track', 'Lead');
+          fbq('track', 'CompleteRegistration');
+
+      </script>
+      <noscript>
+          <img height="1" width="1" style="display:none"
+               src="https://www.facebook.com/tr?id=707913527708593&ev=PageView&noscript=1"/>
+      </noscript>
+      <!-- End Facebook Pixel Code -->
+      <!-- Facebook Pixel Code -->
+    <script>
+          !function (f, b, e, v, n, t, s) {
+              if (f.fbq) return;
+              n = f.fbq = function () {
+                  n.callMethod ?
+                      n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+              };
+              if (!f._fbq) f._fbq = n;
+              n.push = n;
+              n.loaded = !0;
+              n.version = '2.0';
+              n.queue = [];
+              t = b.createElement(e);
+              t.async = !0;
+              t.src = v;
+              s = b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t, s)
+          }(window, document, 'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '866584681288483');
+          fbq('track', 'PageView');
+          fbq('track', 'Lead');
+          fbq('track', 'CompleteRegistration');
+
+      </script>
+      <noscript>
+          <img height="1" width="1" style="display:none"
+               src="https://www.facebook.com/tr?id=866584681288483&ev=PageView&noscript=1"/>
+      </noscript>
+      <!-- End Facebook Pixel Code -->
+      <!-- Facebook Pixel Code -->
+    <script>
+          !function (f, b, e, v, n, t, s) {
+              if (f.fbq) return;
+              n = f.fbq = function () {
+                  n.callMethod ?
+                      n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+              };
+              if (!f._fbq) f._fbq = n;
+              n.push = n;
+              n.loaded = !0;
+              n.version = '2.0';
+              n.queue = [];
+              t = b.createElement(e);
+              t.async = !0;
+              t.src = v;
+              s = b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t, s)
+          }(window, document, 'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '864753631435090');
+          fbq('track', 'PageView');
+          fbq('track', 'Lead');
+          fbq('track', 'CompleteRegistration');
+
+      </script>
+      <noscript>
+          <img height="1" width="1" style="display:none"
+               src="https://www.facebook.com/tr?id=864753631435090&ev=PageView&noscript=1"/>
+      </noscript>
+      <!-- End Facebook Pixel Code -->
 </head>
 
 <body>
